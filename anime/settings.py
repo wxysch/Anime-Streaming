bab435df0e1f41cac41b0602fc1dfa06e36144c1
alinu1 <uwu1343@gmail.com>
2023-04-03 16:40:46 +0600

    add new app

--- anime/settings.py
+++ anime/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
     #apps
     'apps.settings',
+    'apps.movie',
 ]
 
 MIDDLEWARE = [
--- anime/urls.py
+++ anime/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('apps.settings.urls'))
+    path('',include('apps.settings.urls')),
+    path('',include('apps.movie.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
